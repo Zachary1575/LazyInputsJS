f1a93a059371c87c5bd07a7fa393919980172fd3
Zachary <75510412+Zachary1575@users.noreply.github.com>
2022-08-29 23:01:28 -0400

    Created base React Component
    Created Base React Component

---
+++ src/src/lazyInputs.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class lazyInputs extends Component {
+    render() {
+        return (
+            <>
+                <h1>Hello!</h1>
+                <input type="text"></input>
+            </>
+        );
+    }
+}
+
+export default lazyInputs;
